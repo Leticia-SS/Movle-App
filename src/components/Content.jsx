55066eba715f2c7b99fc212fe2f9dafa81ac00ea
Leticia-SS <leticia.saraiva013@gmail.com>
2024-09-27 09:02:11 -0300

    style: ajust background image

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,5 +1,4 @@
 import Card from "./Card"
-import Background from '../assets/background.svg'
 import './Components.css'
 
 const Content = () => {
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,7 @@
 const Footer = () => {
-    return(
+    return (
         <footer>
-            <h2>Sobre o App</h2>
-            <p>Este projeto consiste na criação de um site interativo que combina entretenimento de jogos com o mundo do cinema. O site oferecerá diferentes modos de jogos temáticos, todos baseados em filmes, utilizando uma API de filmes para gerar conteúdo dinâmico e atualizado. A ideia central é proporcionar aos usuários uma experiência envolvente, desafiando seus conhecimentos sobre filmes de forma divertida e diversificada.</p>
+            <p>&#169; Todos os Direitos Reservados</p>
         </footer>
     )
 }
